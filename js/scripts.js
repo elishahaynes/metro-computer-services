4f374f9fc137078f506468d28361d05939cd1d1b
Eli Haynes <elishahaynes@gmail.com>
2020-07-14 22:32:20 -0400

    added slogan typewriter function

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+// Hamburger Menu JavaScript
+
+
+// function myFunction() {
+//     var x = document.getElementById("homeNav");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+
+// function myFunction() {
+//     var x = document.getElementById("servicesNav");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+
+// function myFunction() {
+//     var x = document.getElementById("blogNav");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+
+// function myFunction() {
+//     var x = document.getElementById("contactNav");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+
+
+
+// Testing the Typewriter Function below 
+
+// let i = 0;
+// let txt = 'Serving the IT needs of Louisville since 2008';
+// let speed = 50;  The speed/duration of the effect in milliseconds
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("typing").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
