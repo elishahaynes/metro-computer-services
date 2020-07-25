edb9a0e4d7730a578d822b45f2584a711fb69e2d
Eli Haynes <elishahaynes@gmail.com>
2020-07-25 17:42:43 -0400

    add to README.md and added comments throughout

---
+++ README.md
@@ -0,0 +1,18 @@
+Metro Computer Services Website
+
+Welcome to my final project for the Front End Web Development course at Code Louisville! This is a website that will eventually be a fully functioning site for the local IT company my Partner owns. The final goal of this website is to convey the services that he provides, a way to contact him, and a place to house his blog content. 
+
+Below are the 3 "feature requirements" I have implemented:
+
+1. A navigation menu that collapses into a responsive mobile-friendly menu ("hamburger" menu).               */This was implemented with the main navigation menu/*
+2. Use media queries to switch from a one-column mobile layout to a two or more column desktop layout.       */This was implemented with the Personal & Business Services columns/*
+3. Create an image carousel with at least 3 images that it rotates through.                                  */This was implemented with the testimonials at the bottom of the page/*
+
+All of the "additional requirements" should have been met.
+
+
+*Sources I used for hamburger menu:
+    Fellow Thursday night Code Louisville student Brian Kiefer, who recommended watching this YouTube video by TubeMint: https://www.youtube.com/watch?v=8r3hGIvyP3k
+
+
+*Sources I used for 
--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,25 @@
-// Hamburger Menu JavaScript
+// Hamburger Menu (one of the "feature requirements")
+
+function toggleClass(){
+  let menu = document.querySelector(".main-nav");
+  menu.classList.toggle("toggleCls");
+}
 
 
-// function myFunction() {
-//     var x = document.getElementById("homeNav");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
+// Carousel of Testimonials (one of the "feature requirements")
 
-// function myFunction() {
-//     var x = document.getElementById("servicesNav");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
-
-// function myFunction() {
-//     var x = document.getElementById("blogNav");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
-
-// function myFunction() {
-//     var x = document.getElementById("contactNav");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
-
-
-
-// Testing the Typewriter Function below 
-
-// let i = 0;
-// let txt = 'Serving the IT needs of Louisville since 2008';
-// let speed = 50;  The speed/duration of the effect in milliseconds
-// function typeWriter() {
-//   if (i < txt.length) {
-//     document.getElementById("typing").innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
-// }
-
-
-
-var slideIndex = 0;
+let slideIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
+  let i;
+  let x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 8000); // Change image every 8 seconds
+  setTimeout(carousel, 8000); // Changes image every 8 seconds
 }
 
-
-function toggleClass(){
-    let menu = document.querySelector(".main-nav");
-    menu.classList.toggle("toggleCls");
-}
